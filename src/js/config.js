c9858e758eaa3d94675d387df6f73e2f903ecce1
Sherif Ali <elemlak202099@gmail.com>
2025-08-22 16:27:03 +0300

    First Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '472b7309-e6b5-45c0-9ef2-a712837749fa';
+export const MODAL_CLOSE_SEC = 2.5;
